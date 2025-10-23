fa582ea089ecd0dd1c40a45f1646855b098461ac
abarouski-dev <abarouski@edu.cdv.pl>
2025-10-23 17:48:58 +0200

    Commit 1023

---
+++ Source/Project/Public/ABasePlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ABaseCharacter.h"
+#include "ABasePlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AABasePlayerCharacter : public AABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
